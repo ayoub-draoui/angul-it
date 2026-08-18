279ae48f852b1e653bec95b3a06f37871e762d60
adraoui <mr.stefan.draoui@gmail.com>
2026-08-18 19:03:16 +0100

    models

--- Angul-It/src/app/models/challenges.model.ts
+++ Angul-It/src/app/models/challenges.model.ts
@@ -5,8 +5,24 @@ export enum ChallengeType {
 }
 
 export interface Challenge {
-  id: string;
-  type: ChallengeType;
-  question: string;
-  points: number;
+  id: string
+  type: ChallengeType
+  question: string
+  points: number
+}
+
+export interface ImageSelectionChallenge  {
+        id : string 
+        type: ChallengeType.IMAGE_SELECTION;
+        questtion: string;
+        points: number;
+        // images: ImageOption[];
+}
+
+  export interface MathChallenge  {
+        id : string ;
+        type: ChallengeType.MATH;
+        question: string;
+        points: number;
+        answer: number;
 }
